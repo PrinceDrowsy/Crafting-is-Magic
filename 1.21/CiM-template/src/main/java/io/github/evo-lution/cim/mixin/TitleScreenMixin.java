89f5c1a73833d632b2fdd64c4047964a0f48ae42
James Montgomery <erenroseyeager@gmail.com>
2024-12-19 15:02:07 -0500

    Quilt Template 1.21 added

---
+++ 1.21/CiM-template/src/main/java/io/github/evo-lution/cim/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package io.github.evo-lution.cim.mixin;
+
+import io.github.evo-lution.cim.CiM;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    @Inject(method = "init", at = @At("TAIL"))
+    public void onInit(CallbackInfo ci) {
+        CiM.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
